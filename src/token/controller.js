372babb961c1b4af535b04677510b931adf29eea
Solomon <63205747+solomonNSI@users.noreply.github.com>
2023-10-31 01:51:09 +0300

    fix/ move to arbitrum goerli

--- src/token/controller.js
+++ src/token/controller.js
@@ -6,150 +6,196 @@ const {Web3} = require('web3'); // Crypto imports
 const EthereumTx = require('ethereumjs-tx').Transaction;
 const Binance = require('node-binance-api');
 const Common = require('ethereumjs-common').default;
-const { LegacyTransaction } = require('@ethereumjs/tx');
 
-const web3 = new Web3(process.env.INFURA)
-
-// smart contract initialization
-const sc_Address = process.env.SC_ADDRESS;
+const web3 = new Web3(process.env.INFURA_ARBITRUM_TESTNET);
+const sc_Address = process.env.SC_TESTNET;
 const sc_ABI = require('./xUSDabi.json');
-const contract = new web3.eth.Contract(sc_ABI, sc_Address);
+const contract = new web3.eth.Contract(sc_ABI, sc_Address); // smart contract initialization
 
-// wallet initialization
-const priv_Key = Buffer.from(process.env.ACC_PRIV_ADDRESS, 'hex');
-const signer = web3.eth.accounts.privateKeyToAccount(priv_Key);
+const priv_Key = Buffer.from(process.env.ACC_PRIV_ADDRESS, 'hex'); 
+const signer = web3.eth.accounts.privateKeyToAccount(priv_Key); // wallet initialization
 
-// binance
 const binance = new Binance().options({
     APIKEY: process.env.BINANCE_API_KEY,
     APISECRET: process.env.BINANCE_API_SECRET,
     'family': 4,
 })
 
-router.post("/mint", async (req, res) => {
+const common1 = Common.forCustomChain(
+    'mainnet', {
+        name: 'arbitrum',
+        networkId: 42161,
+        chainId: 42161,
+    },
+    'petersburg',
+);
+
+const common = Common.forCustomChain(
+    'mainnet', {
+        name: 'arbitrum-goerli',
+        networkId: 421613,
+        chainId: 421613,
+    },
+    'petersburg',
+);
+
+async function getTokenBalance(walletAddress) {
     try {
-        const { receiverAddress, amount, amountOfETH, priceOfEth } = req.body;
-        /* 
-        *  ------------------------------------------------------------------
-        *   Binance Part
-        *  ------------------------------------------------------------------  
-        */
+        const balance = await contract.methods.balanceOf(walletAddress).call();
+        return balance;
+    } catch (error) {
+        console.error('Error checking token balance:', error);
+        throw error;
+    }
+}
 
-        // Define the chain configuration
-        const common = Common.forCustomChain(
-            'mainnet', {
-                name: 'arbitrum-goerli',
-                networkId: 42161,
-                chainId: 42161,
-            },
-            'petersburg',
-        );
-        console.info(req.body);         
-        if (!amountOfETH || !priceOfEth) { 
-            return res.status(400).json({ error: "Price of eth and amount of eth not found" });
-        }
-
-        // Transfer amountOfEth from spot to coin-m
-        console.info(await binance.universalTransfer("MAIN_CMFUTURE", "ETH", amountOfETH));
-
-        // 1 cont is 10 dollars on binance, need to adjust the price accordingly
-        const ONE_CONT = parseFloat(( 10 / priceOfEth).toFixed(5));
-        const CONT_AMOUNT = parseInt((amountOfETH / ONE_CONT).toFixed(1));
-        console.info("CONT_AMOUNT", CONT_AMOUNT);
-        const position = await binance.deliveryMarketSell("ETHUSD_PERP", CONT_AMOUNT); // Opening a position
-        console.info("CONT_AMOUNT2", CONT_AMOUNT);
-        /* 
-        *  ------------------------------------------------------------------
-        *   Minting Part
-        *  ------------------------------------------------------------------  
-        */
-        if (!receiverAddress || !amount) { 
-            return res.status(400).json({ error: "Receiver address and amount are required." });
-        }
-    
-        const amountInWei = web3.utils.toWei(amount.toString(), 'ether'); // Convert the amount to a BigNumber (Wei)
-        const nonce = await web3.eth.getTransactionCount(signer.address); // Fetch the current nonce for the sender address
+async function transferETH(address, amount) {
+    try {
+        const amountInWei = web3.utils.toWei(amount.toString(), 'ether');
+        const nonce = await web3.eth.getTransactionCount(signer.address);
         
-        const rawTransaction = {                // Create a raw transaction object
+        const gasEstimate = await contract.methods.mint(address, amountInWei).estimateGas();
+        const rawTransaction = {
             nonce: web3.utils.toHex(nonce),
             gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
-            gasLimit: web3.utils.toHex(300000), // Adjust the gas limit as needed
+            gasLimit: web3.utils.toHex(gasEstimate),
+            to: address,
+            value: amountInWei, // Convert amount to Wei
+            data: '0x', // Empty data for a simple ETH transfer
+          };
+
+        const tx = new EthereumTx(rawTransaction, { common });
+        tx.sign(priv_Key);
+
+        const serializedTx = tx.serialize();
+        const txHash = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
+        return txHash;
+    } catch (error){
+        console.error('Error sending back ETH');
+        throw error;
+    }
+}
+
+async function processTokenTransaction(address, amount, method) {
+    try {
+        const amountInWei = web3.utils.toWei(amount.toString(), 'ether');
+        const nonce = await web3.eth.getTransactionCount(signer.address);
+        
+        const gasEstimate = await contract.methods.mint(address, amountInWei).estimateGas();
+      
+        const rawTransaction = {
+            nonce: web3.utils.toHex(nonce),
+            gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
+            gasLimit: web3.utils.toHex(gasEstimate),
             to: sc_Address,
-            data: contract.methods.mint(receiverAddress, amountInWei).encodeABI(),
+            data: method === 'mint' ? 
+                contract.methods.mint(address, amountInWei).encodeABI() 
+                : 
+                contract.methods.burn(address, amountInWei).encodeABI(),
         };
 
-        const tx = new EthereumTx(rawTransaction, {common}); // Use the appropriate chain
-        tx.sign(priv_Key);    // Sign the transaction
-    
-        const serializedTx = tx.serialize(); // Serialize the transaction and send it
+        const tx = new EthereumTx(rawTransaction, { common });
+        tx.sign(priv_Key);
+
+        const serializedTx = tx.serialize();
         const txHash = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
-        
-        return res.status(200).json({ transactionHash: txHash.transactionHash});
+        return txHash;
+
     } catch (error) {
-        console.error('Error minting tokens:', error);
+        console.error(`Error ${method === 'mint' ? 'minting' : 'burning'} tokens:`, error);
+        throw error; // Rethrow the error
+    }
+}
+
+async function processBinance(amountOfETH, priceOfEth, method, burnerAddress) {
+    const ONE_CONT = 10; // 1 cont is 10 dollars on Binance
+    const ONE_CONT_IN_ETH = ONE_CONT / priceOfEth;
+
+    const SPOT_TO_COINM_TRANSFER_FEE = 0.0009; // Fee for transferring from spot to coin-m
+    try {
+        // Calculate the number of conts
+        const contAmount = (amountOfETH / ONE_CONT_IN_ETH).toFixed(0);
+        console.log("cont", contAmount);
+        if (method === 'open') {
+            // Transfer amountOfETH from spot to coin-m
+            await binance.universalTransfer('MAIN_CMFUTURE', 'ETH', amountOfETH).then( async() => {
+                // Opening a position by selling ETHUSD_PERP contracts
+                console.log(await binance.deliveryMarketSell('ETHUSD_PERP', contAmount));
+            });
+        } else {
+            // Closing a position by buying ETHUSD_PERP contracts
+            await binance.deliveryMarketBuy('ETHUSD_PERP', contAmount).then(async () => {
+                // Transfer ETH back to the spot account, subtracting the transfer fee
+                await binance.universalTransfer('CMFUTURE_MAIN', 'ETH', amountOfETH - SPOT_TO_COINM_TRANSFER_FEE);
+            })
+            console.log(burnerAddress);
+            
+            // For MVP, we do not withdraw. If you want to enable withdrawal, you can use the following code:
+            /*
+            const client2 = binance({
+                apiKey: process.env.BINANCE_API_KEY,
+                apiSecret: process.env.BINANCE_API_SECRET,
+            });
+
+            console.log('withdraw', await client2.withdraw({
+                coin: 'ETH',
+                network: 'ARBITRUM',
+                address: burnerAddress,
+                amount: amountOfETH - 0.0002, // You can adjust the withdrawal amount
+            }));
+            */
+        }
+    } catch (error) {
+        console.error(`Error ${method === 'open' ? 'opening' : 'closing'} position:`, error);
+        throw error;
+    }
+}
+
+router.post("/mint", async (req, res) => {
+    const { receiverAddress, amount, amountOfETH, priceOfEth } = req.body;
+    try {
+        if (!amountOfETH || !priceOfEth || !receiverAddress || !amount) { return res.status(404).json({ error: "Arguments missing" }); }
+
+        const minting_result = await processTokenTransaction(receiverAddress, amount, 'mint');
+        
+        setTimeout(async () => {
+            try {
+                processBinance(amountOfETH, priceOfEth, 'open', '');
+            } catch (error) {
+                await processTokenTransaction(receiverAddress, amount, 'burn');
+                return res.status(501).json({error: "An error occured while opening a position"});
+            }
+        }, 2000);
+        return res.status(200).json({txHash: minting_result.transactionHash})
+    } catch (error) {
+        console.error(error);
         return res.status(500).json({ error: "An error occurred while minting tokens." });
     }
-  });
+});
 
 router.post("/burn", async (req, res) => {
     try {
         const { burnerAddress, amount, amountOfETH, priceOfEth } = req.body;
-        if (!burnerAddress || !amount) { return res.status(400).json({ error: "Burner address and amount are required." });}
-        console.info(req.body);
-
-        const amountInWei = web3.utils.toWei(amount.toString(), 'ether'); // Convert the amount to a BigNumber (Wei)
-        const nonce = await web3.eth.getTransactionCount(signer.address); // Fetch the current nonce for the sender address
+        if (!amountOfETH || !priceOfEth || !burnerAddress || !amount) { return res.status(404).json({error: "Arguments missing"});}
         
-        const rawTransaction = { // Create a raw transaction object
-            nonce: web3.utils.toHex(nonce),
-            gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
-            gasLimit: web3.utils.toHex(300000), // Adjust the gas limit as needed
-            to: sc_Address,
-            data: contract.methods.burn(burnerAddress, amountInWei).encodeABI(),
-        };
-        
-        const common = Common.forCustomChain(
-            'mainnet', {
-                name: 'arbitrum-goerli',
-                networkId: 42161,
-                chainId: 42161,
-            },
-            'petersburg',
-        );
-
-        const tx = new EthereumTx(rawTransaction, {common}); // Use the appropriate chain
-        tx.sign(priv_Key); // Sign the transaction
-  
-        const serializedTx = tx.serialize(); // Serialize the transaction and send it
-        const txHash = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
-        
-        // Opening a position
-        // 1 cont is 10 dollars, so we got to calculate how much eth that is
-        if (!amountOfETH || !priceOfEth) { return res.status(400).json({ error: "Price of eth and amount of eth not found" });}
-
-        const ONE_CONT = parseFloat(( 10 / priceOfEth).toFixed(5));
-        const CONT_AMOUNT = parseInt((amountOfETH / ONE_CONT).toFixed(1));
-        console.info("CONT_AMOUNT", CONT_AMOUNT);
-        console.info(await binance.deliveryMarketBuy("ETHUSD_PERP", CONT_AMOUNT));
-        await binance.universalTransfer("CMFUTURE_MAIN", "ETH", amountOfETH - 0.0008);
-
-        const Binance = require('binance-api-node').default;
-        const client2 = Binance({
-            apiKey: process.env.BINANCE_API_KEY,
-            apiSecret: process.env.BINANCE_API_SECRET,
-        })
-        console.log( await client2.withdraw({
-            coin: 'ETH',
-            network: 'ARBITRUM',
-            address: burnerAddress,
-            amount: amountOfETH - 0.0008,
+        getTokenBalance(burnerAddress)
+            .then(async (balance) => {
+                const balanceInEther = web3.utils.fromWei(balance, 'ether'); // Convert from Wei to Ether
+                if (amount > balanceInEther) {return res.status(500).json({error: "Insufficient balance"}); }
+                // Process the ETH transfer to the burnerAddress
+                ethTransferHash = await transferETH(burnerAddress, amountOfETH);
+                console.log(`ETH transfer transaction hash: ${ethTransferHash}`);
             })
-        );
-        return res.status(200).json({ transactionHash: txHash.transactionHash});
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        
+        processBinance(amountOfETH, priceOfEth, 'close', burnerAddress);
+        const burning_result = await processTokenTransaction(burnerAddress, amount, 'burn');
+        return res.status(200).json({ txHash: burning_result.transactionHash});
     } catch(error) {
-        console.error("Error burning tokens: ", error);
         return res.status(500).json({error: "An error occured while burning tokens. "});
     }
-})
+});
 
 module.exports = router;
